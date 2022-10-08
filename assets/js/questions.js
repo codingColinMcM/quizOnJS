eb9235961a5508b2afd327f8b0f2357efe162bff
Colin McMurtray <112663656+codingColinMcM@users.noreply.github.com>
2022-10-07 22:26:22 -0400

    Update questions.js

--- assets/js/questions.js
+++ assets/js/questions.js
@@ -1,1 +1,25 @@
+var questions = {
 
+    "Commonly used data types in JavaScript include all of the following EXCEPT: ":
+    {
+        "Strings": false,
+        "Booleans": false,
+        "Alerts": true,
+        "Integers": false,
+    },
+
+    "The condition of a conditional statement is enclosed within: ":
+    {
+        "Quotation marks": false,
+        "Parenthases": true,
+        "Curly Brackets": false,
+        "Box Brackets": false,
+    }
+
+}
+
+function displayQuestions() {
+    for (const property in questions) {
+        console.log(`${property}: ${questions[property]}`);
+      }
+}
