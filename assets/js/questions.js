eb438f60b9da36244297f2028e27b2c40d2b3825
Colin McMurtray <112663656+codingColinMcM@users.noreply.github.com>
2022-10-11 22:38:12 -0400

    Update questions.js

--- assets/js/questions.js
+++ assets/js/questions.js
@@ -2,14 +2,37 @@ var questionTitle = document.getElementById("question-title");
 var questionSection= document.getElementById("questions");
 var startButton = document.getElementById("start"); 
 var choiceDiv = document.getElementById("choices");
-
-
+var time = document.getElementById("time");
+var displayScore = document.getElementById("final-score");
 
 startButton.addEventListener("click", displayQuestions);
-
+startButton.addEventListener("click", timer);
 
 var newQuestion = false;
 var i = 0;
+var numCorrect = 0;
+var percentScore = 0;
+
+var sec = 4;
+function timer(){
+    
+    setInterval(function(){
+        sec--;
+
+        if(sec==0) {
+        
+            displayQuestions();            
+        
+        }
+
+        if (sec < 0) {
+
+            clearInterval(timer);
+        
+        }
+    }, 1000);
+}
+// decrementAfter1Second();
 
 var questions = {
 
@@ -31,44 +54,52 @@ var questions = {
 
 }
 
+var numQuestions = Object.keys(questions).length;
+
 function displayQuestions() {
+     
+    if ((i < numQuestions) & (sec > 0)) {
 
-    var currentTitle = Object.keys(questions)[i];
-    questionTitle.innerHTML=currentTitle;
+        var currentTitle = Object.keys(questions)[i];
+        questionTitle.innerHTML=currentTitle;
 
-    for (var choice in questions[currentTitle]) {
+        for (var choice in questions[currentTitle]) {
 
-        var choiceValue = questions[currentTitle][choice];
-        var questionChoice = document.createElement("button");
-        questionChoice.innerHTML = choice;
+            var choiceValue = questions[currentTitle][choice];
+            var questionChoice = document.createElement("button");
+            questionChoice.innerHTML = choice;
 
-        if (choiceValue == false){
-            
-            questionChoice.className = "wrong-choice";
-            questionChoice.id = "wrong-choice"
-            
-        }else {
+            if (choiceValue == false){
+                
+                questionChoice.className = "wrong-choice";
+                questionChoice.id = "wrong-choice"
+                
+            }else {
 
-            questionChoice.className = "right-choice";
-            questionChoice.id = "right-choice"
+                questionChoice.className = "right-choice";
+                questionChoice.id = "right-choice"
+
+            }
+
+            choiceDiv.appendChild(questionChoice);
+            var addBr = document.createElement("br");
+            addBr.id = "question-br";
+            choiceDiv.appendChild(addBr)
 
         }
 
-        choiceDiv.appendChild(questionChoice);
-        var addBr = document.createElement("br");
-        addBr.id = "question-br";
-        choiceDiv.appendChild(addBr)
+        i++;
+
+        var wrongAnswer = document.getElementById("wrong-choice");
+        wrongAnswer.addEventListener("click", displayWrong);
+        var correctAnswer = document.getElementById("right-choice");
+        correctAnswer.addEventListener("click", displayCorrect)
+
+    } else {  
+
+        calcScore()
 
     }
-
-    i++;
-
-    console.log("before click");
-
-    var wrongAnswer = document.getElementById("wrong-choice");
-    wrongAnswer.addEventListener("click", displayWrong);
-    var correctAnswer = document.getElementById("right-choice");
-    correctAnswer.addEventListener("click", displayCorrect)
 
 }
 
@@ -80,14 +111,29 @@ function removeOldQuestions() {
         choiceDiv.removeChild(oldIncorrect);
         choiceDiv.removeChild(choiceBreak)
     }
+
     var oldCorrect = document.getElementById("right-choice");
     choiceDiv.removeChild(oldCorrect);
+    var choiceBreak = document.getElementById("question-br");
+    choiceDiv.removeChild(choiceBreak)
+
+}
+
+function removeOldWrong() {
+
+    var oldWrong = document.getElementById("wrong-button");
+    choiceDiv.removeChild(oldWrong)
+
+}
+
+function removeOldRight() {
+
+    var oldRight = document.getElementById("right-button");
+    choiceDiv.removeChild(oldRight)
 
 }
 
 function displayWrong() {
-
-    console.log("did it work?");
 
     removeOldQuestions();
 
@@ -95,20 +141,32 @@ function displayWrong() {
     wrongButton.innerHTML = "WRONG";
     wrongButton.className = "wrong-button";
     wrongButton.id = "wrong-button";
-    wrongButton.addEventListener("click", displayQuestions)
+    wrongButton.addEventListener("click", displayQuestions);
+    wrongButton.addEventListener("click", removeOldWrong)
+    choiceDiv.appendChild(wrongButton);
 
 }
 
 function displayCorrect() {
-
-    console.log("did it work?");
 
     removeOldQuestions();
 
     var rightButton = document.createElement("button");
     rightButton.innerHTML = "CORRECT";
     rightButton.className = "right-button";
-    rightButton.className = "right-button";
-    rightButton.addEventListener("click", displayQuestions)
+    rightButton.id = "right-button";
+    rightButton.addEventListener("click", displayQuestions);
+    rightButton.addEventListener("click", removeOldRight)
+    choiceDiv.appendChild(rightButton);
+
+    numCorrect++;
     
+}
+
+function calcScore() {
+    
+    var floatScore = numCorrect/numQuestions;
+    percentScore = Math.round(floatScore * 100);
+    displayScore.innerHTML = percentScore
+
 }
